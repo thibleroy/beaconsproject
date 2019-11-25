964200a7256a5a3cf898f6e1fe937582736432f2
issarompion <irompion@yahoo.fr>
2019-11-25 16:54:23 +0100

    progress beacon

--- app/src/app/beacon-list/beacon-list.component.ts
+++ app/src/app/beacon-list/beacon-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {BeaconScannerService} from "../../services/beacon-scanner.service";
+import { Platform, Events } from '@ionic/angular';
+import { IBeacon } from "@ionic-native/ibeacon/ngx";
 
 @Component({
   selector: 'app-beacon-list',
@@ -8,10 +9,85 @@ import {BeaconScannerService} from "../../services/beacon-scanner.service";
 })
 export class BeaconListComponent implements OnInit {
 
-  constructor(beaconScanner: BeaconScannerService) { }
+  delegate: any;
+      region: any;
+      constructor(
+        public platform: Platform,
+        public events: Events,
+        private iBeacon: IBeacon
+      ) {
+         this.initialise(); 
+      }
 
   ngOnInit() {
+    //this.startExploring()
+  }
 
+  startExploring (){
+  
+    // Request permission to use location on iOS
+this.iBeacon.requestAlwaysAuthorization();
+// create a new delegate and register it with the native layer
+let delegate = this.iBeacon.Delegate();
+
+// Subscribe to some of the delegate's event handlers
+delegate.didRangeBeaconsInRegion()
+.subscribe(
+data => console.log('didRangeBeaconsInRegion: ', data),
+error => console.error()
+);
+delegate.didStartMonitoringForRegion()
+.subscribe(
+data => console.log('didStartMonitoringForRegion: ', data),
+error => console.error()
+);
+delegate.didEnterRegion()
+.subscribe(
+data => {
+  console.log('didEnterRegion: ', data);
+}
+);
+
+let beaconRegion = this.iBeacon.BeaconRegion('deskBeacon','F7826DA6-ASDF-ASDF-8024-BC5B71E0893E');
+
+this.iBeacon.startMonitoringForRegion(beaconRegion)
+.then(
+() => console.log('Native layer received the request to monitoring'),
+error => console.error('Native layer failed to begin monitoring: ', error)
+);
+  }
+
+  initialise(): any {
+    let promise = new Promise((resolve, reject) => {
+      if (this.platform.is("cordova")) {
+        this.iBeacon.requestAlwaysAuthorization(); 
+       // ALSO try this one too this.iBeacon.requestWhenInUseAuthorization();
+        this.delegate = this.iBeacon.Delegate();
+        this.delegate.didRangeBeaconsInRegion().subscribe(
+          data => {
+            this.events.publish("didRangeBeaconsInRegion", data);
+           //console.log("didRangebeacons__" + JSON.stringify(data)); // empty beacons array
+          },
+          error => console.error()
+        );
+        this.region = this.iBeacon.BeaconRegion("thib", "12345678-9101-1121-3141-516171819203");
+        this.iBeacon
+          .startRangingBeaconsInRegion(this.region)
+          .then(
+            () => {
+              resolve(true);
+            },
+            error => {
+              console.error("Failed to begin monitoring: ", error);
+              resolve(false);
+            }
+          );
+      } else {
+        resolve(false);
+      }
+    });
+
+    return promise;
   }
 
 }
--- app/src/services/beacon-scanner.service.ts
+++ app/src/services/beacon-scanner.service.ts
@@ -6,38 +6,42 @@ import {IBeacon} from '@ionic-native/ibeacon/ngx';
 })
 export class BeaconScannerService {
 
-  constructor(private ibeacon: IBeacon) {
+    constructor(private ibeacon: IBeacon) {
 
-    // Request permission to use location on iOS
-    this.ibeacon.requestAlwaysAuthorization();
+    }
+  
+     startExploring (){
+  
+        // Request permission to use location on iOS
+this.ibeacon.requestAlwaysAuthorization();
 // create a new delegate and register it with the native layer
-    let delegate = this.ibeacon.Delegate();
+let delegate = this.ibeacon.Delegate();
 
 // Subscribe to some of the delegate's event handlers
-    delegate.didRangeBeaconsInRegion()
-        .subscribe(
-            data => console.log('didRangeBeaconsInRegion: ', data),
-            error => console.error()
-        );
-    delegate.didStartMonitoringForRegion()
-        .subscribe(
-            data => console.log('didStartMonitoringForRegion: ', data),
-            error => console.error()
-        );
-    delegate.didEnterRegion()
-        .subscribe(
-            data => {
-              console.log('didEnterRegion: ', data);
-            }
-        );
+delegate.didRangeBeaconsInRegion()
+  .subscribe(
+    data => console.log('didRangeBeaconsInRegion: ', data),
+    error => console.error()
+  );
+delegate.didStartMonitoringForRegion()
+  .subscribe(
+    data => console.log('didStartMonitoringForRegion: ', data),
+    error => console.error()
+  );
+delegate.didEnterRegion()
+  .subscribe(
+    data => {
+      console.log('didEnterRegion: ', data);
+    }
+  );
 
-    let beaconRegion = this.ibeacon.BeaconRegion('deskBeacon','F7826DA6-ASDF-ASDF-8024-BC5B71E0893E');
+let beaconRegion = this.ibeacon.BeaconRegion('deskBeacon','F7826DA6-ASDF-ASDF-8024-BC5B71E0893E');
 
-    this.ibeacon.startMonitoringForRegion(beaconRegion)
-        .then(
-            () => console.log('Native layer received the request to monitoring'),
-            error => console.error('Native layer failed to begin monitoring: ', error)
-        );
-
-  }
+this.ibeacon.startMonitoringForRegion(beaconRegion)
+  .then(
+    () => console.log('Native layer received the request to monitoring'),
+    error => console.error('Native layer failed to begin monitoring: ', error)
+  );
+      }
+  
 }
