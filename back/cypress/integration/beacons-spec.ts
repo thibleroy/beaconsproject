3c85ccf108fe274693a08711009d069e2c8ed6ce
thibleroy <thibault.leroy.1@etudiant.com>
2019-12-10 20:25:58 +0100

    add, delete and list beacon

--- back/cypress/integration/beacons-spec.ts
+++ back/cypress/integration/beacons-spec.ts
@@ -2,6 +2,24 @@ import {addBeacon, deleteBeacon, getBeacons} from "../requests/beacon";
 import {Beacon} from "../../src/entities/interfaces";
 const testBeacon: Beacon = {uuid: 'uuidtest2',minor:  123,major: 456,id_client: 'idtest',name: 'nametest'};
 
+describe('add beacon', function() {
+    before(() => {
+        getBeacons().then((resp: Cypress.Response) => {
+            resp.body.beacons.forEach((beacon: any) => {
+                if (beacon.name === 'nametest'){
+                    deleteBeacon(beacon.id);
+                }
+            });
+        });
+    });
+    it('adds a beacon', () => {
+        addBeacon(testBeacon).then((resp: Cypress.Response) => {
+            console.log(resp.body);
+            expect(resp.body.status).to.eq(true);
+        });
+    });
+});
+
 describe('delete beacon', () => {
     before(() => {
         addBeacon(testBeacon);
@@ -19,11 +37,10 @@ describe('delete beacon', () => {
 
     })
 });
-/*
 describe('get beacons', function() {
     before(() => {
         addBeacon(testBeacon);
-    })
+    });
     it('gets beacons', function() {
         getBeacons().then((resp: Cypress.Response) => {
             expect(resp.body.status).to.eq(true);
@@ -38,7 +55,7 @@ describe('delete beacon', () => {
         getBeacons().then((resp: Cypress.Response) => {
             resp.body.beacons.forEach((beacon: Beacon) => {
                 if (beacon.name === 'nametest'){
-                    deleteBeacon(beacon).then((resp: Cypress.Response) => {
+                    deleteBeacon(beacon.id).then((resp: Cypress.Response) => {
                         expect(resp.body.status).to.eq(true);
                     });
                 }
@@ -47,17 +64,7 @@ describe('delete beacon', () => {
 
     })
 });
-describe('add beacon', function() {
-    before(() => {
-        deleteBeacon(testBeacon);
-    });
-    it('adds a beacon', () => {
-        addBeacon(testBeacon).then((resp: Cypress.Response) => {
-            console.log(resp.body);
-            expect(resp.body.status).to.eq(true);
-        });
-    });
-});
+
 describe('add existing beacon', () => {
     before(() => {
         addBeacon(testBeacon);
@@ -67,4 +74,4 @@ describe('add existing beacon', () => {
             expect(resp.body.status).to.eq(false);
         })
     })
-}); */
+});
