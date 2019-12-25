d3984495ffbb09ba207476adacde795b9e8af258
thibleroy <thibault.leroy.1@etudiant.com>
2019-12-26 00:53:16 +0100

    cont

--- back/src/functions/express.functions.ts
+++ back/src/functions/express.functions.ts
@@ -1,9 +1,20 @@
 import {Request, Response} from "express";
 import * as log4js from 'log4js';
+log4js.configure({
+    appenders: {
+        logstash: {
+            type: '@log4js-node/logstashudp',
+            host: '127.0.0.1',
+            port: 5000
+        }
+    },
+    categories: {
+        default: { appenders: ['logstash'], level: 'info' }
+    }
+});
 const logger = log4js.getLogger();
-logger.level = 'debug';
-
 export function timeLog(req: Request, res: Response, next: any) {
-    logger.debug(`${res.statusCode} ${req.method} ${req.originalUrl} | ${req.headers["user-agent"]}`);
+    console.log(`${res.statusCode} ${req.method} ${req.originalUrl} | ${req.headers["user-agent"]}`);
+    logger.info('Information !', {time: Date.now()});
     next();
 }
