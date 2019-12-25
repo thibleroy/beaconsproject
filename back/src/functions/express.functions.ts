5e9cee8f3f61e13d3024b383380b3410d9e8585c
thibleroy <thibault.leroy.1@etudiant.com>
2019-12-25 23:44:54 +0100

    ok innit

--- back/src/functions/express.functions.ts
+++ back/src/functions/express.functions.ts
@@ -1,6 +1,9 @@
 import {Request, Response} from "express";
+import * as log4js from 'log4js';
+const logger = log4js.getLogger();
+logger.level = 'debug';
 
 export function timeLog(req: Request, res: Response, next: any) {
-    console.log(`${req.method} : ${JSON.stringify(req.body)} to ${req.originalUrl}`);
+    logger.debug(`${res.statusCode} ${req.method} ${req.originalUrl} | ${req.headers["user-agent"]}`);
     next();
 }
