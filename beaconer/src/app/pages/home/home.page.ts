864acc84a5e02eb7bd2cb43a6487e18608256e68
issarompion <irompion@yahoo.fr>
2020-01-28 09:21:05 +0100

    update

--- beaconer/src/app/pages/home/home.page.ts
+++ beaconer/src/app/pages/home/home.page.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy, NgZone } from '@angular/core';
 import { Router,ActivatedRoute } from '@angular/router';
-
+import {BeaconService} from '../../services/beacon.service';
+import {HttpService} from '../../services/http.service';
+import {Events, Platform} from '@ionic/angular';
+import {Beacon} from '@ionic-native/ibeacon/ngx';
+import {IBeacon} from '../../../../../back/src/entities/interfaces';
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
@@ -8,13 +13,118 @@ import { Router,ActivatedRoute } from '@angular/router';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  
+  beacons : IBeacon[]
   id_client:string;
+  zone: NgZone;
+  nearest_beacon_id : string
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private beaconService: BeaconService,
+    private httpService : HttpService,
+    public platform: Platform,
+    private events: Events,
+    public toastController: ToastController
+    ) {
+    this.id_client = this.route.snapshot.queryParams["id_client"];
+     }
 
   ngOnInit() {
-  this.id_client = this.route.snapshot.queryParams["id_client"];
+    this.httpService.getBeacons(this.id_client)
+    .subscribe(cr =>{
+      this.beacons = cr.value
+      this.handleBeacon()
+    })
+  }
+
+  handleBeacon(){
+    if (this.platform.is('cordova') && Array.isArray(this.beacons) && this.beacons.length) {
+        this.zone = new NgZone({ enableLongStackTrace: false });
+        let uuid = this.beacons[0].uuid
+        this.beaconService.initialise(uuid).then(() => {
+            this.events.subscribe('didRangeBeaconsInRegion', (data) => {
+                this.zone.run(() => {
+
+                  let beacon_id : string = this.get_beacon_id(this.get_nearest_beacon(data.beacons))
+
+                  if(beacon_id){
+                    if(beacon_id !=this.nearest_beacon_id){
+                      this.ShowToast()
+                    }
+                  }
+
+                });
+            });
+        });
+      }
+      
+      /*else{
+        this.ShowToast()
+      }*/
+  }
+
+  get_beacon_id(beacon:Beacon) : string {
+    for (var i = 0; i <= this.beacons.length; i ++) {
+      if(i < this.beacons.length){
+          if(
+            beacon.uuid == this.beacons[i].uuid &&
+            beacon.major == this.beacons[i].major &&
+            beacon.minor == this.beacons[i].minor
+            ){
+              return this.beacons[i].id_beacon
+          }
+      }else{
+          return undefined
+      }
+    }
+  }
+
+  get_nearest_beacon(beacons:Beacon[]) : Beacon{
+    let res : Beacon
+    for (var i = 0; i <= beacons.length; i ++) {
+      if(i < beacons.length){
+        if(res){
+          if(beacons[i].rssi>res.rssi){
+            res = beacons[i]
+          }
+        }else{
+          res = beacons[i]
+        }
+      }else{
+        return res
+      }
+    }
+  }
+
+  async ShowToast() {
+  const toast = await this.toastController.create({
+    header: 'Notification',
+    message: "Vous êtes à proximité d'un beacon",
+    position: 'top',
+    buttons: [
+      {
+        side: 'start',
+        icon: 'star',
+        text: 'Voir',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Done',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }
+    ]
+  });
+  toast.present();
+  }
+
+  ngOnDestroy() {
+    if (this.platform.is('cordova')) {
+      this.beaconService.stopRanging();
+    }
   }
 
 }
--- beaconer/src/app/services/beacon.service.ts
+++ beaconer/src/app/services/beacon.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {Events, Platform} from '@ionic/angular';
 import {IBeacon, IBeaconDelegate, IBeaconPluginResult, BeaconRegion} from '@ionic-native/ibeacon/ngx';
 import {Observable} from 'rxjs';
-import { ToastController } from "@ionic/angular";
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +13,7 @@ export class BeaconService {
   constructor(
       public platform: Platform,
       private iBeacon: IBeacon,
-      public events: Events,
-      public toastController: ToastController
+      public events: Events
   ) { 
     if (this.platform.is('cordova')) {
       this.iBeacon.requestAlwaysAuthorization();
@@ -23,22 +21,9 @@ export class BeaconService {
       // this.iBeacon.disableDebugLogs();
     }
   }
-
-startRanging(): Promise<void> {
-    return this.iBeacon.startRangingBeaconsInRegion(this.iBeacon.BeaconRegion('thib', '12345678-9101-1121-3141-516171819203'));
-}
-
-stopRanging(): Promise<void> {
-    return this.iBeacon.stopRangingBeaconsInRegion(this.iBeacon.BeaconRegion('thib', '12345678-9101-1121-3141-516171819203'));
-}
-
-getBeacons(): Observable<IBeaconPluginResult> {
-    return this.delegate.didRangeBeaconsInRegion();
-}
-
-initialise(): Promise<boolean> {
-  return new Promise((resolve => {
-      if (this.platform.is('cordova')) {
+initialise(uuid:string): Promise<boolean> {
+    return new Promise((resolve => {
+      if (this.platform.is('cordova') && uuid) {
           /* Request permission to use location on iOS */
           this.iBeacon.requestAlwaysAuthorization();
 
@@ -51,37 +36,23 @@ initialise(): Promise<boolean> {
               }
           );
           this.iBeacon.startRangingBeaconsInRegion(
-              this.iBeacon.BeaconRegion('thib', '12345678-9101-1121-3141-516171819203'))
+              this.iBeacon.BeaconRegion('activiot', uuid))
               .then(() => {
               resolve(true);
           });
       }
-  }));
+    }));
 }
 
-async presentToastWithOptions() {
-  const toast = await this.toastController.create({
-    header: 'Toast header',
-    message: 'Click to Close',
-    position: 'top',
-    buttons: [
-      {
-        side: 'start',
-        icon: 'star',
-        text: 'Favorite',
-        handler: () => {
-          console.log('Favorite clicked');
-        }
-      }, {
-        text: 'Done',
-        role: 'cancel',
-        handler: () => {
-          console.log('Cancel clicked');
-        }
-      }
-    ]
-  });
-  toast.present();
+stopRanging(): Promise<void> {
+  return this.iBeacon.stopRangingBeaconsInRegion(this.iBeacon.BeaconRegion('thib', '12345678-9101-1121-3141-516171819203'));
 }
+
+/*startRanging(): Promise<void> {
+    return this.iBeacon.startRangingBeaconsInRegion(this.iBeacon.BeaconRegion('thib', '12345678-9101-1121-3141-516171819203'));
+}
+getBeacons(): Observable<IBeaconPluginResult> {
+    return this.delegate.didRangeBeaconsInRegion();
+}*/
 
 }
