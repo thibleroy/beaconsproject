71e4a4daa89fb0f4dca0dfdb3319dda7a86e737c
maximecarpier <maxime.carpier@gmail.com>
2020-02-03 16:23:45 +0100

    routes

--- back/microservices/src/beacon/src/index.ts
+++ back/microservices/src/beacon/src/index.ts
@@ -1,13 +1,16 @@
 import * as instances from "msconnector";
-import {Consumer, ConsumerOptions, Message} from "msconnector/node_modules/kafka-node";
-import {ENV} from "lib";
-import {BeaconMessage} from 'msconnector/IMessage';
-import {IBeacon} from "lib";
-import {BeaconModel} from "@src/beacon/src/Beacon";
-const consumerOptions: ConsumerOptions = {fromOffset: false};
+import { Consumer, ConsumerOptions, Message } from "msconnector/node_modules/kafka-node";
+import { ENV } from "lib";
+import { BeaconMessage } from 'msconnector/IMessage';
+import { IBeacon } from "lib";
+import { BeaconModel } from "@src/beacon/src/Beacon";
+import mongoose from 'mongoose';
+const consumerOptions: ConsumerOptions = { fromOffset: false };
+
 const authConsumer: Consumer = new Consumer(instances.kafkaClient, ['' + ENV.kafka_topic_beacon], consumerOptions);
+
 authConsumer.on('message', async (message: Message) => {
-    const data: BeaconMessage  = JSON.parse(message.value.toString());
+    const data: BeaconMessage = JSON.parse(message.value.toString());
 
     switch (data.type) {
 
@@ -15,17 +18,37 @@ authConsumer.on('message', async (message: Message) => {
 
             switch (data.action) {
 
+                case 'list':
+                    BeaconModel.find({ id_client: data.value.id_client }, function (err: any, docs: IBeacon[]) {
+                        sendKafkaResponse(docs, 'list')
+                    });
+                    break;
+
                 case 'create':
-                    const beacon: IBeacon = data.value;
+                    let id_client: number = data.value.id_client;
+                    let beacon = new BeaconModel(data.value);
+
+                    beacon.save(function (err: any, beacon: IBeacon) {
+                        if (err) return console.error(err);
+                        console.log(" saved to bookstore collection.");
+                    });
+                    sendKafkaResponse('', 'create')
+                    break;
+
+                case 'get':
+                    BeaconModel.find({ id_client: data.value.id_client, id_beacon: data.value }, function (err: any, docs: IBeacon) {
+                        sendKafkaResponse(docs, 'get')
+                    });
                     break;
 
                 case 'delete':
+                    BeaconModel.deleteOne({ id_client: data.value.id_client }, function (err: any) {
 
+                    });
+                    sendKafkaResponse('', 'delete')
                     break;
-                case 'get':
-                    break;
+
                 case 'update':
-
                     break;
                 default: break;
             }
@@ -35,3 +58,12 @@ authConsumer.on('message', async (message: Message) => {
 
     }
 });
+
+function sendKafkaResponse(val: any, action: String) {
+    const messageValue: BeaconMessage = { type: "res", action: action, value: val };
+
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_beacon, messages: messageValue }], (err: Error, data: any) => {
+        console.log('send producer clients - get all beacons-', data);
+    });
+}
+
--- back/microservices/src/webserver/src/routes/route.ts
+++ back/microservices/src/webserver/src/routes/route.ts
@@ -1,96 +1,118 @@
 import * as express from "express";
 import { Router } from "express";
 import { Request, Response } from "express";
-import {BeaconMessage, ClientMessage} from "msconnector/IMessage";
+import { BeaconMessage, ClientMessage } from "msconnector/IMessage";
 import * as instances from '../../api_connector.js';
-import {ENV} from "lib";
+import { ENV } from "lib";
 import { IBeacon } from "lib";
 import { IClient } from "lib";
-import {Message} from "msconnector/node_modules/kafka-node";
+import { Message } from "msconnector/node_modules/kafka-node";
 
 const router: Router = express.Router();
 
-instances.apiConsumer.on('message', async (message: Message) => {
-    const data: BeaconMessage  = JSON.parse(message.value.toString());
-});
+var response: Response;
 
 //Routes Clients
 
 router.get('/', async (req: Request, res: Response) => {
-    const ClientMsg: ClientMessage = {type: 'req', action: 'list'};
-    instances.apiProducer.send([{topic: '' + ENV.kafka_topic_client, messages: ''}], (err: Error, data: any) => {
+    const ClientMsg: ClientMessage = { type: 'req', action: 'list' };
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_client, messages: '' }], (err: Error, data: any) => {
         console.log('send producer clients - get all clients-', data);
+        response = res;
     });
 });
 
 router.post('/', async (req: Request, res: Response) => {
     const currentClient: IClient = req.body;
-    const ClientMsg: ClientMessage = {type: 'req', action: 'create', value: currentClient};
-    instances.apiProducer.send([{topic: '' + ENV.kafka_topic_client, messages: ClientMsg}], (err: Error, data: any) => {
+    const ClientMsg: ClientMessage = { type: 'req', action: 'create', value: currentClient };
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_client, messages: ClientMsg }], (err: Error, data: any) => {
         console.log('send producer clients - create a client-', data);
+        response = res;
     });
 });
 
 router.get('/:clientId', async (req: Request, res: Response) => {
-    const ClientMsg: ClientMessage = {type: 'req', action: 'get', value: {id_client: req.params.clientId}};
-    instances.apiProducer.send([{topic: '' + ENV.kafka_topic_client, messages: ClientMsg}], (err: Error, data: any) => {
+    const ClientMsg: ClientMessage = { type: 'req', action: 'get', value: { id_client: req.params.clientId } };
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_client, messages: ClientMsg }], (err: Error, data: any) => {
         console.log('send producer clients - get a client -', data);
+        response = res;
     });
 });
 
 router.delete('/:clientId', async (req: Request, res: Response) => {
-    const ClientMsg: ClientMessage = {type: 'req', action: 'delete', value: {id_client: req.params.clientId}};
-    instances.apiProducer.send([{topic: '' + ENV.kafka_topic_client, messages: ClientMsg}], (err: Error, data: any) => {
+    const ClientMsg: ClientMessage = { type: 'req', action: 'delete', value: { id_client: req.params.clientId } };
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_client, messages: ClientMsg }], (err: Error, data: any) => {
         console.log('send producer clients - delete a client -', data);
+        response = res;
     });
 });
 
 router.put('/:clientId', async (req: Request, res: Response) => {
     const currentClient: IClient = req.body;
-    const ClientMsg: ClientMessage = {type: 'req', action: 'update', value: currentClient};
-    instances.apiProducer.send([{topic: '' + ENV.kafka_topic_client, messages: ClientMsg}], (err: Error, data: any) => {
+    const ClientMsg: ClientMessage = { type: 'req', action: 'update', value: currentClient };
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_client, messages: ClientMsg }], (err: Error, data: any) => {
         console.log('send producer clients - delete a client -', data);
+        response = res;
     });
 });
 
 //Routes Beacons
 
 router.get('/:clientId/beacons', async (req: Request, res: Response) => {
-    const beaconMsg: BeaconMessage = {type: 'req', action: 'list', value: {id_client: req.params.clientId}};
-    instances.apiProducer.send([{topic: '' + ENV.kafka_topic_beacon, messages: beaconMsg}], (err: Error, data: any) => {
+    const beaconMsg: BeaconMessage = { type: 'req', action: 'list', value: { id_client: req.params.clientId } };
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_beacon, messages: beaconMsg }], (err: Error, data: any) => {
         console.log('send producer clients - get all beacons-', data);
+        response = res;
     });
 });
 
 router.post('/:clientId/beacons/:beaconId', async (req: Request, res: Response) => {
     const currentBeacon: IBeacon = req.body;
-    const beaconMsg: BeaconMessage = {type: 'req', action: 'create', value: currentBeacon};
-    instances.apiProducer.send([{topic: '' + ENV.kafka_topic_beacon, messages: beaconMsg}], (err: Error, data: any) => {
+    const beaconMsg: BeaconMessage = { type: 'req', action: 'create', value: currentBeacon };
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_beacon, messages: beaconMsg }], (err: Error, data: any) => {
         console.log('send producer clients - create a beacon -', data);
+        response = res;
     });
 });
 
 router.get('/:clientId/beacons/:beaconId', async (req: Request, res: Response) => {
-    const beaconMsg: BeaconMessage = {type: 'req', action: 'get', value: {id_client: req.params.clientId, id_beacon: req.params.clientId}};
-    instances.apiProducer.send([{topic: '' + ENV.kafka_topic_beacon, messages: beaconMsg}], (err: Error, data: any) => {
+    const beaconMsg: BeaconMessage = { type: 'req', action: 'get', value: {id_client : req.params.clientId, id_beacon : req.params.beaconId} };
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_beacon, messages: beaconMsg }], (err: Error, data: any) => {
         console.log('send producer clients - get a beacon -', data);
+        response = res;
     });
 });
 
 router.delete('/:clientId/beacons/:beaconId', async (req: Request, res: Response) => {
-    const beaconMsg: BeaconMessage = {type: 'req', action: 'delete', value: {id_client: req.params.clientId}};
-    instances.apiProducer.send([{topic: '' + ENV.kafka_topic_beacon, messages: beaconMsg}], (err: Error, data: any) => {
+    const beaconMsg: BeaconMessage = { type: 'req', action: 'delete', value: { id_client: req.params.clientId } };
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_beacon, messages: beaconMsg }], (err: Error, data: any) => {
         console.log('send producer clients - delete a beacon -', data);
+        response = res;
     });
 });
 
 router.put('/:clientId/beacons/:beaconId', async (req: Request, res: Response) => {
     const currentClient: IClient = req.body;
-    const beaconMsg: BeaconMessage = {type: 'req', action: 'update', value: currentClient};
-    instances.apiProducer.send([{topic: '' + ENV.kafka_topic_beacon, messages: beaconMsg}], (err: Error, data: any) => {
-        console.log('send producer clients - delete a beacon -', data);
+    const beaconMsg: BeaconMessage = { type: 'req', action: 'update', value: currentClient };
+    instances.apiProducer.send([{ topic: '' + ENV.kafka_topic_beacon, messages: beaconMsg }], (err: Error, data: any) => {
+        console.log('send producer clients - update a beacon -', data);
+        response = res;
     });
 });
+
+/*instances.apiConsumer.on('message', async (message: Message) => {
+    switch (JSON.parse(message.value.toString())) {
+        case 'res':
+            response.send(message.value.value) 
+            break;
+    
+        default:
+            break;
+    }
+});*/
+
+
+
 
 exports.beaconsRouter = router;
 
