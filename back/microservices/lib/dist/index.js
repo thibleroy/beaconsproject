e2a235397814ee861f5a02205b3393e1dbc32c89
issarompion <irompion@yahoo.fr>
2020-02-04 18:05:58 +0100

    fix nodes_modules

--- back/microservices/lib/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const kafka_node_1 = require("kafka-node");
-const lib_1 = require("lib/");
-console.log(lib_1.ENV.kafka_port);
-const clientOptions = { kafkaHost: `${lib_1.ENV.kafka_url}:${lib_1.ENV.kafka_port}` };
-exports.kafkaClient = new kafka_node_1.KafkaClient(clientOptions);
