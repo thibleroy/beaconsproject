d134bdeda5997143aa30564a1d1c701d88a09162
issarompion <irompion@yahoo.fr>
2020-02-07 13:54:21 +0100

    update file

--- beaconer/cypress/integration/examples/main.spec.js
+++ beaconer/cypress/integration/examples/main.spec.js
@@ -1,28 +1,16 @@
   describe("Tests cliuents", () => {
     beforeEach(() => {
-      cy.visit("/");
-    });
-    it("get List of client", () => {
-    cy.server();
-    cy.route({
+      cy.server();
+      cy.route({
         method: 'GET',
         url: 'http://localhost:3000/clients',
         status: 200,
         response: 'fixture:clients.json'
       }).as('List')
+    });
+    it("get List of client", () => {
+    cy.visit("/");
     cy.wait("@List");
     cy.get("[data-cy=ListClients]").should("be.visible").first().click();
     });
   })
-
-
-/*it("Client", () => {
-      cy.server();
-      cy.route({
-          method: 'GET',
-          url: 'http://localhost:3000/client/1',
-          status: 200,
-          response: 'fixture:client.json'
-        });
-      });
-  });*/
