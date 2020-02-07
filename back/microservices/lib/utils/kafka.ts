c0cd23214af8d76f254f7586059d4870e883f5ac
issarompion <irompion@yahoo.fr>
2020-02-07 14:37:56 +0100

    add content to msg kafka

--- back/microservices/lib/utils/kafka.ts
+++ back/microservices/lib/utils/kafka.ts
@@ -1,9 +1,9 @@
 import 'module-alias/register';
 import {ENV} from "lib";
 import {Producer} from '../node_modules/kafka-node'
-import { BeaconMessage, ClientMessage, AuthMessage } from "../IMessage";
+import { BeaconMessage, ClientMessage, AuthMessage, ContentMessage } from "../IMessage";
 
-export const sendKafkaMessage = ( prod: Producer, topicVal: string, msg: BeaconMessage | ClientMessage | AuthMessage ) => {
+export const sendKafkaMessage = ( prod: Producer, topicVal: string, msg: BeaconMessage | ClientMessage | AuthMessage | ContentMessage  ) => {
     
        prod.send([{ topic: topicVal, messages: msg }], (err: Error, data: any) => {
             console.log('send producer', data);
